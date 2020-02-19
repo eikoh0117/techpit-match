a39786b8676cc6fddc63cef86eb7dbe64a1036ac
Hidemitsu Aoki <olympos0117@gmail.com>
2020-02-19 08:26:02 +0000

    [Add]before introduce jTinder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
 
 def after_sign_in_path_for(resource) 
-  user_path(current_user.id)
+  users_path
 end
 def after_sign_out_path_for(resource) 
   root_path
 end
 def after_account_update_path_for(resource)
-  user_path(current_user.id)
+  users_path
 end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  def show
-    # @user = User.find(params[:id])
-    @user = User.find_by(id: params[:id])
+
+  def index
+    @users = User.where.not(id: current_user.id)
   end
+
+  def show
+    @user = User.find(params[:id])
+    # @user = User.find_by(id: params[:id])
+  end
+
   private
+  
   def user_params
     params.require(:user).permit(:name, :self_introduction,:sex, :image_name)
   end
