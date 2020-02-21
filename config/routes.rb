b461c303319a56f06b0c719a4fe0aab2468e1ae0
Hidemitsu Aoki <olympos0117@gmail.com>
2020-02-21 13:41:55 +0000

    [Add]Matching functions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # get 'top/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index]
+  resources :reactions, only: [:create]
+  resources :matching, only: [:index]
   root "top#index"
 end
